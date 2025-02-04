9edd7466bbc9429915cc522d3ec1e1b766175e67
François Pagé <francois.page@mail.mcgill.ca>
2025-02-03 19:27:56 -0500

    ok so quitting works on other file but when theres user read input its really weird

--- lua/core/plugin_config/nvim-dap-ui.lua
+++ lua/core/plugin_config/nvim-dap-ui.lua
@@ -1,21 +1,21 @@
 -- Debugging Support
-require('dapui').setup({})
+require("dapui").setup({})
 
 -- Add event listeners for DAP UI
-local dap = require('dap')
+local dap = require("dap")
 
 dap.listeners.after.event_initialized["dapui_config"] = function()
-  require('dapui').open()
+	require("dapui").open()
 end
 
 dap.listeners.before.event_terminated["dapui_config"] = function()
-  -- Commented to prevent DAP UI from closing when unit tests finish
-  -- require('dapui').close()
+	-- Commented to prevent DAP UI from closing when unit tests finish
+	-- require('dapui').close()
 end
 
 dap.listeners.before.event_exited["dapui_config"] = function()
-  -- Commented to prevent DAP UI from closing when unit tests finish
-  -- require('dapui').close()
+	-- Commented to prevent DAP UI from closing when unit tests finish
+	-- require('dapui').close()
 end
 
 -- Add dap configurations based on your language/adapter settings
@@ -25,5 +25,13 @@ end
 --   },
 -- }
 
-
-
+vim.api.nvim_create_autocmd("BufWinLeave", {
+	pattern = "dap-repl",
+	callback = function()
+		local dap_repl = require("dap.repl")
+		if dap_repl then
+			print("[DAP] Closing REPL...")
+			pcall(dap_repl.close) -- Ensure it doesn't crash if nil
+		end
+	end,
+})
