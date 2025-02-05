d9f9068566702b88360d0f463392174e64172ab0
François Pagé <francois.page@mail.mcgill.ca>
2025-02-05 17:35:06 -0500

    bufferline setup added so one less bug? everytime to logs. Same for lualine. though g:bufferline fails

--- lua/core/plugin_config/init.lua
+++ lua/core/plugin_config/init.lua
@@ -1,4 +1,6 @@
 require("core.plugin_config.colorscheme")
+
+require("core.plugin_config.bufferline")
 require("core.plugin_config.lualine")
 require("core.plugin_config.treesitter")
 require("core.plugin_config.telescope")
--- lua/core/plugin_config/lualine.lua
+++ lua/core/plugin_config/lualine.lua
@@ -1,14 +1,23 @@
-require('lualine').setup {
-  options = {
-    icons_enabled = true,
-    theme = 'nightfly',
-  },
-  sections = {
-    lualine_a = {
-      {
-        'filename',
-        path = 1,
-      }
-    }
-  }
-}
+require("lualine").setup({
+	options = {
+		icons_enabled = true,
+		theme = "nightfly",
+	},
+	sections = {
+		lualine_a = {
+			{
+				"filename",
+				path = 1,
+			},
+		},
+		lualine_b = {
+			{
+				"branch",
+				icon = "",
+				cond = function()
+					return vim.fn.system("git rev-parse --is-inside-work-tree 2>/dev/null") == "true\n"
+				end,
+			},
+		},
+	},
+})
