8f372b716ad0a42c5963e34119d1428112b4c7df
François Pagé <francois.page@mail.mcgill.ca>
2025-02-23 20:35:48 -0500

    ?

--- lua/core/plugin_config/mini_surround.lua
+++ lua/core/plugin_config/mini_surround.lua
@@ -3,15 +3,32 @@ require("mini.surround").setup({
 	-- Add custom surroundings to be used on top of builtin ones. For more
 	-- information with examples, see `:h MiniSurround.config`.
 	custom_surroundings = {
-		["r"] = {
-			output = { left = "\27[31m", right = "\27[0m" }, -- Red ANSI
-		},
-		["g"] = {
-			output = { left = "\27[32m", right = "\27[0m" }, -- Green ANSI
-		},
-		["b"] = {
-			output = { left = "\27[34m", right = "\27[0m" }, -- Blue ANSI
-		},
+		-- Standard Colors
+		["r"] = { output = { left = "\27[31m", right = "\27[0m" } }, -- Red
+		["g"] = { output = { left = "\27[32m", right = "\27[0m" } }, -- Green
+		["y"] = { output = { left = "\27[33m", right = "\27[0m" } }, -- Yellow
+		["b"] = { output = { left = "\27[34m", right = "\27[0m" } }, -- Blue
+		["m"] = { output = { left = "\27[35m", right = "\27[0m" } }, -- Magenta
+		["c"] = { output = { left = "\27[36m", right = "\27[0m" } }, -- Cyan
+		["w"] = { output = { left = "\27[37m", right = "\27[0m" } }, -- White
+		["k"] = { output = { left = "\27[30m", right = "\27[0m" } }, -- Black
+
+		-- Bright Colors
+		["R"] = { output = { left = "\27[91m", right = "\27[0m" } }, -- Bright Red
+		["G"] = { output = { left = "\27[92m", right = "\27[0m" } }, -- Bright Green
+		["Y"] = { output = { left = "\27[93m", right = "\27[0m" } }, -- Bright Yellow
+		["B"] = { output = { left = "\27[94m", right = "\27[0m" } }, -- Bright Blue
+		["M"] = { output = { left = "\27[95m", right = "\27[0m" } }, -- Bright Magenta
+		["C"] = { output = { left = "\27[96m", right = "\27[0m" } }, -- Bright Cyan
+		["W"] = { output = { left = "\27[97m", right = "\27[0m" } }, -- Bright White
+
+		-- Styles
+		["i"] = { output = { left = "\27[3m", right = "\27[0m" } }, -- Italic
+		["u"] = { output = { left = "\27[4m", right = "\27[0m" } }, -- Underline
+		["s"] = { output = { left = "\27[9m", right = "\27[0m" } }, -- Strikethrough
+		["o"] = { output = { left = "\27[1m", right = "\27[0m" } }, -- Bold
+		["d"] = { output = { left = "\27[2m", right = "\27[0m" } }, -- Dim
+		["h"] = { output = { left = "\27[7m", right = "\27[0m" } }, -- Inverted (Highlight)
 	},
 
 	-- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
