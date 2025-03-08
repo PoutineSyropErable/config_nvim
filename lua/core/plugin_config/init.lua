24c57eb6894845966b871e393c4e2b51a46d1e0a
François Pagé <francois.page@mail.mcgill.ca>
2025-03-08 05:14:42 -0500

    huh?

---
+++ lua/core/plugin_config/init.lua
@@ -0,0 +1,56 @@
+require("core.plugin_config.alpha")
+require("core.plugin_config.colorscheme")
+require("core.plugin_config.coloriser")
+
+require("core.plugin_config.nvimtree_config")
+require("core.plugin_config.oil")
+require("core.plugin_config.treesitter")
+require("core.plugin_config.telescope")
+require("core.plugin_config.neoclip")
+
+require("core.plugin_config.mason")
+require("core.plugin_config.lsp_config")
+require("core.plugin_config.completions")
+require("core.plugin_config.conform")
+require("core.plugin_config.black")
+require("core.plugin_config.nvim-dap-virtual-text")
+require("core.plugin_config.nvim-dap-config")
+require("core.plugin_config.fugitive")
+
+require("core.plugin_config.toggleterm")
+require("core.plugin_config.markview")
+require("core.plugin_config.image")
+
+require("core.plugin_config.mini_surround")
+require("core.plugin_config.gitsigns")
+require("core.plugin_config.tressj")
+
+require("core.plugin_config.swagger-preview")
+require("core.plugin_config.vim-test")
+require("core.plugin_config.colors-highlight")
+
+require("core.plugin_config.bufferline")
+
+require("core.plugin_config.barbar")
+
+require("core.plugin_config.lualine")
+
+require("core.plugin_config.nvim-possession")
+
+------ ##### DEACTIVATED ##### -------
+
+-- not needed vv (hologram) (and markdown_preview)
+-- require("core.plugin_config.hologram")
+-- require("core.plugin_config.markdown_preview")
+
+-- Completions are in completions.lua, not nvim.cmp
+-- require("core.plugin_config.nvim-cmp")
+
+-- require("core.plugin_config.EXTRA_DEBUG")
+
+-- require("core.plugin_config.tabout")
+-- require("core.plugin_config.noice")
+
+-- End of file
+
+require("core.plugin_config.last")
