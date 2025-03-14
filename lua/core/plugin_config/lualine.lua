6d5c9340cf85d0586d3b1453dcb5d9caaf857f82
François Pagé <francois.page@mail.mcgill.ca>
2025-03-14 01:45:33 -0400

    added recording for lualine, so noice breaking @recording is fine then2

--- lua/core/plugin_config/lualine.lua
+++ lua/core/plugin_config/lualine.lua
@@ -1,3 +1,17 @@
+local getMacro = function()
+	-- Check if a macro is being recorded and show the register
+	if vim.fn.reg_recording() ~= "" then
+		return "Recording  @" .. vim.fn.reg_recording()
+	else
+		return ""
+	end
+end
+
+local getTime = function()
+	-- Show the current time formatted as "HH:MM"
+	return os.date("%Hh:%Mm")
+end
+
 require("lualine").setup({
 	options = {
 		icons_enabled = true,
@@ -13,12 +27,12 @@ require("lualine").setup({
 			},
 		},
 		-- lualine_a = { "mode" },
-		-- lualine_b = { "branch", "diff", "diagnostics" },
+		lualine_b = { "branch", "diff", "diagnostics" },
 		lualine_c = {
 			{ "tabs", mode = 1 }, -- Show buffer names in the tabline
 		},
-		-- lualine_x = { "encoding", "fileformat", "filetype" },
-		-- lualine_y = { "progress" },
-		-- lualine_z = { "location" },
+		lualine_x = { getMacro, "encoding", "fileformat", "filetype" },
+		lualine_y = { "progress", getTime },
+		lualine_z = { "location" },
 	},
 })
