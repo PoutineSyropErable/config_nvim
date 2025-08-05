ad3afe820cffd9f87d50bcb7535c368d5faa6b32
François Pagé <francois.page@mail.mcgill.ca>
2025-08-05 18:20:46 -0400

    please work

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -58,4 +58,17 @@ require("lazy").setup({
 		config = function() require("treesitter_inspect").setup() end,
 		-- lazy = false  -- Remove comment if you want immediate loading
 	},
+	{
+		"PoutineSyropErable/nvim-possession", --- TODO, make my own
+		branch = "main", -- Ensure you're using the correct branch
+		dependencies = {
+			"ibhagwan/fzf-lua",
+			{
+				"tiagovla/scope.nvim",
+				lazy = false,
+				config = true,
+			},
+		},
+		config = true,
+	},
 })
